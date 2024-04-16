b6d5cfa9fb0f573e5f743c3539798f75757e73a3
Manas0410 <manasshrivastava0410@gmail.com>
2024-04-17 01:27:35 +0530

    upcoming tym , validation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,28 @@
 "use client";
+import { useGetCalls } from "@/Hooks/useGetCalls";
 import MeetingTypeList from "@/components/ui/MeetingTypeList";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
   const [time, setTime] = useState("");
   const [date, setDate] = useState("");
+
+  const { upcomingCalls } = useGetCalls();
+  console.log(upcomingCalls, "upcoming");
+
+  const [upcomingTime, setUpcomingTime] = useState<string>("");
+  useEffect(() => {
+    if (!upcomingCalls[upcomingCalls.length - 1]) return;
+    setUpcomingTime(
+      (
+        upcomingCalls[upcomingCalls.length - 1] as Call
+      ).state?.startsAt?.toLocaleString() ||
+        (
+          upcomingCalls[upcomingCalls.length - 1] as unknown as CallRecording
+        ).start_time?.toLocaleString()
+    );
+  }, [upcomingCalls]);
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
@@ -35,8 +53,8 @@ export default function Home() {
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Upcoming meeting : 11 : 11
+          <h2 className="glassmorphism max-w-[370px] rounded py-2 text-center text-base font-normal">
+            Upcoming meeting : {upcomingTime}
           </h2>
           <div className="flex flex-col gap-2">
             <h2 className="text-4xl font-extrabold lg:text-7xl ">{time}</h2>
