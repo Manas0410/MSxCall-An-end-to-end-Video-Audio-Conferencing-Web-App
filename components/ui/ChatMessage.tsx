a45f2924bfd2c8763b618a1f0476f2608d3b4064
Manas0410 <manasshrivastava0410@gmail.com>
2024-04-21 17:41:28 +0530

    chat app v1

--- components/ui/ChatMessage.tsx
+++ components/ui/ChatMessage.tsx
@@ -9,7 +9,7 @@ const ChatMessage = ({ userId, time, message }: msgPropType) => {
   return (
     <div>
       <div className="flex justify-start gap-4 items-center mb-[4px]">
-        <div className="text-[18px] font-[500] text">
+        <div className="text-[18px] font-[500] text" title={userId}>
           {userId?.slice(0, 20)}...{" "}
         </div>
         <div className="text-[12px] font-[400]">{time}</div>
